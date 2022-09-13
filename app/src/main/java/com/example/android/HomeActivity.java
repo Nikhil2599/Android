2bfe84550424f4eceb437b07332b621d88fa34b6
Nikhil.S <nikhils2599@gmail.com>
2022-09-13 19:13:49 +0530

    start-dailer/alarm

--- app/src/main/java/com/example/android/HomeActivity.java
+++ app/src/main/java/com/example/android/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.android;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -10,12 +11,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
     EditText etContact;
+    TextView tvHome;
+    public static String TAG = HomeActivity.class.getSimpleName();
 
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         etContact = findViewById(R.id.etContact);
+        tvHome = findViewById(R.id.tvHome);
+        String name = getIntent().getExtras().getString("nkey");
+        tvHome.setText("hello "+name);
     }
 
     public void handleClicks(View viewClicked) {
--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -1,20 +1,60 @@
 package com.example.android
 
+
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
-
 class MainActivity : AppCompatActivity() {
-    lateinit var etName :EditText
+    lateinit var etName : EditText //declaration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-         etName = findViewById(R.id.etName)
+        setContentView(R.layout.activity_main) //inflated -layoutinflater
+        etName = findViewById(R.id.etName)
+        Log.i(TAG,"im in oncreate")
     }
     fun clickHandler(viewClicked: View) {
+        when(viewClicked.id){
+            R.id.btnLogin -> {startHomeActivity()}
+            R.id.btnDial ->{startDialer()}
+            R.id.btnAlarm -> {createAlarm("sync",7,12)}
+        }
+
+    }
+
+    private fun startDialer() {
+        var dIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:123456"))
+        startActivity(dIntent)
+    }
+
+    private fun startHomeActivity() {
+        Log.e(TAG, "click handler")
+
         var name = etName.text.toString()
-        Toast.makeText(this,name,Toast.LENGTH_SHORT).show()
+        var hIntent = Intent(this, HomeActivity::class.java)
+        hIntent.putExtra("nkey", name)
+
+        startActivity(hIntent)
+    }
+
+    fun createAlarm(message: String, hour: Int, minutes: Int) {
+        val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, message)
+            putExtra(AlarmClock.EXTRA_HOUR, hour)
+            putExtra(AlarmClock.EXTRA_MINUTES, minutes)
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+//https://developer.android.com/guide/components/intents-common#kotlin
+
+    companion object{
+        var TAG = MainActivity::class.java.simpleName
     }
 }
