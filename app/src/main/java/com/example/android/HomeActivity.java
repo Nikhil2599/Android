abce11d28c52af79532637655784ec67a1ec4306
Nikhil.S <nikhils2599@gmail.com>
2022-09-13 16:33:38 +0530

    gettext-from-edittext

--- app/src/main/java/com/example/android/HomeActivity.java
+++ app/src/main/java/com/example/android/HomeActivity.java
@@ -2,21 +2,25 @@ package com.example.android;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
+    EditText etContact;
 
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etContact = findViewById(R.id.etContact);
     }
 
     public void handleClicks(View viewClicked) {
-        Toast.makeText(this,"welcome to android", Toast.LENGTH_SHORT).show();
+        String name = etContact.getText().toString();
+        Toast.makeText(this,name, Toast.LENGTH_SHORT).show();
 
     }
 }
--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var etName :EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+         etName = findViewById(R.id.etName)
     }
     fun clickHandler(viewClicked: View) {
-        Toast.makeText(this,"welcome to android",Toast.LENGTH_SHORT).show()
+        var name = etName.text.toString()
+        Toast.makeText(this,name,Toast.LENGTH_SHORT).show()
     }
 }
